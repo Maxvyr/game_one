53abedd04d43db6560a691705ed45a9d1b43cd92
VIDALINC Maxime <maxvy_@naver.com>
2020-03-12 15:11:03 +0100

    bt start position

--- lib/components/start_text.dart
+++ lib/components/start_text.dart
@@ -5,17 +5,36 @@ import '../game_controller.dart';
 class StartText {
   final GameController gameController;
   TextPainter painter;
+  RRect btStart;
   Offset position;
 
-  StartText(this.gameController) {
+  StartText(this.gameController) {    
+  double widthButton = gameController.screenSize.width*0.5;
+  double heightButton = gameController.screenSize.height*0.1;
+    //texte start
     painter = TextPainter(
       textAlign: TextAlign.center, 
       textDirection: TextDirection.ltr, 
+    );
+    //bouton Start
+    btStart = RRect.fromRectAndRadius(
+      Rect.fromLTWH(
+        gameController.screenSize.width *0.5 - widthButton / 2,
+        gameController.screenSize.height * 0.77 - heightButton / 2,
+        widthButton,
+        heightButton,
+      ),
+      Radius.circular(50.0),
     );
     position = Offset.zero;
   }
 
   void render(Canvas c) {
+    final color =Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 2.0
+      ..color = white;
+    c.drawRRect(btStart, color);
     painter.paint(c, position);
   }
 
@@ -31,7 +50,7 @@ class StartText {
 
       position = Offset(
         (gameController.screenSize.width / 2 - painter.width / 2),
-        (gameController.screenSize.height * 0.7 - painter.width / 2),
+        (gameController.screenSize.height * 0.8 - painter.width / 2),
       );
     }
   }
